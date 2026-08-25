d8dfe5d8ea5ef07b789bd90edad3133d7d4ee34c
Onur Çırakoğlu <onrcrkglu@gmail.com>
2026-08-26 01:23:43 +0300

    Updated first-day.py

--- 01_basics/first-day.py
+++ 01_basics/first-day.py
@@ -2,12 +2,13 @@ print("It wasn't first day really. I learned strings, integers and floats.")
 a = 13
 b = a
 a = 4
+print("I created 2 variables. a=13 and b=a. Then I changed a to 4. Printing a+b will give us 17.")
 print(a+b)
-print("Result must be 17. Because, I changed a after said b=a.")
+print("You can see 17. Now, i am creating one list named deu_cs. It has 4 names in it. Printing deu_cs will show us the list")
 deu_cs = ["Onur", "Ayşe", "Bekir", "Fatma"]
 print(deu_cs)
 deu_cs.append("Yalçın")
-print("I added a new person to my list. Printing again")
+print("I added a new person to my list. This time should be 5 person. Printing again")
 print(deu_cs)
 print("Yalçın must be added to the list.")
 deu_cs.remove("Onur")

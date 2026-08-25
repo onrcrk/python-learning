ac87448b4c5e249c14bfe3592fadb57ad24ed14a
Onur Çırakoğlu <onrcrkglu@gmail.com>
2026-08-26 01:12:35 +0300

    Added first-day.py

---
+++ first-day.py
@@ -0,0 +1,16 @@
+print("It wasn't first day really. I learned strings, integers and floats.")
+a = 13
+b = a
+a = 4
+print(a+b)
+print("Result must be 17. Because, I changed a after said b=a.")
+deu_cs = ["Onur", "Ayşe", "Bekir", "Fatma"]
+print(deu_cs)
+deu_cs.append("Yalçın")
+print("I added a new person to my list. Printing again")
+print(deu_cs)
+print("Yalçın must be added to the list.")
+deu_cs.remove("Onur")
+print("I removed myself from the list. You can't see me on the list after this message.")
+print(deu_cs)
+print("I learned these things before open this repo. We will see where i be at the school opening.\n :)")

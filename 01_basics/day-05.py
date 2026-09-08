a7216c3892f882f0f2735ac816915befdbe3d07f
Onur <onrcrkglu@Onur-MacBook-Air.local>
2026-09-08 21:49:39 +0300

    Updated 'day-05.py'.

---
+++ 01_basics/day-05.py
@@ -0,0 +1,37 @@
+print("Şimdi sözlükler (dictionaries) kısmındayız. Bir sözlük oluşturalım.")
+b = {"marka" : "apple", "model" : "air", "yıl" : 2022, "özellikler" : ["M2 işlemci", "256 hafıza", "16 ram"]}
+print(b)
+print("İlk kelimeler string veya integer olmak zorunda. Adına anahtar (key) diyoruz.\nİkinci kelimelere ise değer (value) adı veriyoruz. Değerler her şey olabilir.")
+print("Sözlükteki marka:", b["marka"])
+
+print("Sözlükteki anahtarları nasıl çağıracağmızı gördük. Şimdi o anahtarı nasıl değiştireceğimizi görelim.")
+b["marka"] = "gamegaraj"
+print("Yeni marka:", b["marka"])
+
+print("Sözlükteki bir değil birden fazla anahtarı değiştirmek istersek şöyle yapmalıyız.")
+b.update({"marka" : "hp", "model" : "victus", "yıl" : 2024})
+print("Güncel sözlük:", b)
+
+print("Yeni bir anahtar eklemek istediğimizde az önce anahtarı değiştirmek için kullandığımız yöntemi kullanacağız.")
+b["klavye"] = "Türkçe Q"
+print(b)
+print("Gördüğünüz üzere 'klavye' anahtarımız eklendi. Ama şimdi silmek istiyorum.")
+del b["klavye"]
+print("Artık sözlüğümüz tekrardan:", b)
+
+print("Sözlüğümdeki anahtarları tek tek görmek için 'for' komutuna başvuruyorum.")
+for x in b:
+    print(x)
+print("Bu bana anahtarları yazdırdı. Şimdi de değerlerini tek tek görmek istiyorum.")
+for y in b:
+    print(b[y])
+
+print("Şimdi anahtları sıralayacak komutumuzu yazıyoruz.")
+print(b.keys())
+print("Bu komut bana anahtarları verdi. Değerler için de aynısını yapalım.")
+print(b.values())
+
+print("Şimdi de sözlüğümüzdeki anahtar ve değerleri tekli bir arada yazdıralım.")
+for k,l in b.items():
+    print(k,":",l)
+print("'items' komutu bize sözlüğümüzdeki anahtar ve değerleri ikili olarak verir.\nBu sayede 'for' döngüsü ile ikili gruplar halinde değerlerimizi tek tek yazdırabiliriz.")
